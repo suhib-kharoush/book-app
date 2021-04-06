f0a15a45cd2f66bf56088bc170cd6e683b870bec
suhib-kharoush <suhaib_kharwash@outlook.com>
2021-04-06 17:50:23 +0300

    almost finish

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 'use strict';
 
 // const { render } = require('ejs');
+require('dotenv').config()
 const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
+const pg = require('pg');
 
 const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3000;
 
 // server.use(express.static('./public'));
-app.get('/', getTasks);
-app.post('/add', addTask);
-app.get('/library/: Id', getSingleTask);
-const pg = require('pg');
 app.use(cors());
 app.use(express.static('./public'));
-const client = new pg.Client(process.env.DATABASE_URL);
-require('dotenv').config()
 app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
-app.get('/', renderHomePage);
-app.get('/searches/new', showForm);
+
+app.get('/', allBooks);
+app.post('/books', addTask);
+app.get('/books/:Id', getSingleTask);
+app.get('/searches/new', showBooks);
 app.post('/searches', createSearch);
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
-app.listen(PORT, () => console.log(`listen on PORT ${PORT}`));
+// app.listen(PORT, () => console.log(`listen on PORT ${PORT}`));
 
 
 function Book(info) {
     // const placeHolderImage = 'https://i.imgur.com/J5LVHEL.jpg';
     this.author = info.authors ? info.authors[0] : 'no authors available'
     this.title = info.title || 'No title available';
-    this.isbn = info.volumeInfo.industryIdentifiers[0].type + info.volumeInfo.industryIdentifiers[0].identifier;
+    this.isbn = info.industryIdentifiers[0].type + info.industryIdentifiers[0].identifier;
     this.img = info.imageLinks ? info.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
     this.description = info.description ? info.description : 'no description available'
 }
@@ -41,11 +42,9 @@ function Book(info) {
 
 
 
-function renderHomePage(req, res) {
-    res.render('pages/index')
-}
 
-function showForm(req, res) {
+
+function showBooks(req, res) {
     res.render('pages/searches/new');
 }
 
@@ -73,9 +72,8 @@ function createSearch(req, res) {
 
 
 
-function getTasks(req, res) {
+function allBooks(req, res) {
     let SQL = `SELECT * FROM library;`;
-    console.log(SQL);
     client.query(SQL).then((results => {
         res.render('pages/index', { results: results.rows });
     })).catch(error => {
@@ -90,7 +88,7 @@ function getTasks(req, res) {
 function getSingleTask(req, res) {
     const id = req.params.Id;
     let SQL = `SELECT * FROM library WHERE id=$1;`;
-    let safeValues = [Id];
+    let safeValues = [id];
 
     client.query(SQL, safeValues).then(results => {
         res.render('pages/books/detail', { result: results.rows })
@@ -101,17 +99,18 @@ function getSingleTask(req, res) {
 
 
 function addTask(req, res) {
-    const sqlQuery = `INSERT INTO books (auther, title, isbn, image_url, description) VALUES($1, $2, $3,$4,$5);`;
+    const sqlQuery = `INSERT INTO library (auther, title, isbn, image_url, description) VALUES($1, $2, $3,$4,$5);`;
     const value = req.body;
     let safeValues = [value.author, value.title, value.isbn, value.image_url, value.description];
     client.query(sqlQuery, safeValues).then((result) => {
-        res.redirect(`/books/${result.rows[0].id}`);
+        const getData = 'SELECT id FROM library WHERE isbn=$1';
+        const safeValues = [value.isbn];
+        client.query(getData, safeValues).then(result => {
+
+            res.redirect(`/books/${result.rows[0].id}`);
+        })
     })
 }
-
-
-
-
 
 
 client.connect().then(() => {
